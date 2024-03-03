843c10e82a27374c17bcfa374d7dcbf4b8ac9240
Brian Hodgman <hodgie@gmail.com>
2024-03-04 08:06:00 +1100

    Refactored policy raters into separate classes and added unit tests

--- ArdalisRating.Tests/RatingEngineRateTests.cs
+++ ArdalisRating.Tests/RatingEngineRateTests.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using System;
 using System.IO;
 using Xunit;
 
@@ -6,14 +7,15 @@ namespace ArdalisRating.Tests
 {
     public class RatingEngineRateTests
     {
+        #region Auto Policy Tests
+
         [Fact]
-        public void ReturnsRatingOf10000For200000LandPolicy()
+        public void ReturnsRatingOf0ForNonBMWMakeAutoPolicy()
         {
             var policy = new Policy
             {
-                Type = PolicyType.Land,
-                BondAmount = 200000,
-                Valuation = 200000
+                Type = PolicyType.Auto,
+                Make = "Mercedes"
             };
             string json = JsonConvert.SerializeObject(policy);
             File.WriteAllText("policy.json", json);
@@ -22,8 +24,50 @@ namespace ArdalisRating.Tests
             engine.Rate();
             var result = engine.Rating;
 
-            Assert.Equal(10000, result);
+            Assert.Equal(0, result);
         }
+
+        [Fact]
+        public void ReturnsRatingOf1000ForBMWMakeAnd200DeductibleAutoPolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Auto,
+                Make = "BMW",
+                Deductible = 200
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
+
+            var engine = new RatingEngine();
+            engine.Rate();
+            var result = engine.Rating;
+
+            Assert.Equal(1000, result);
+        }
+
+        [Fact]
+        public void ReturnsRatingOf900ForBMWMakeAnd500DeductibleAutoPolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Auto,
+                Make = "BMW",
+                Deductible = 500
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
+
+            var engine = new RatingEngine();
+            engine.Rate();
+            var result = engine.Rating;
+
+            Assert.Equal(900, result);
+        }
+
+        #endregion
+
+        #region Land Policy Tests
 
         [Fact]
         public void ReturnsRatingOf0For200000BondOn260000LandPolicy()
@@ -43,5 +87,70 @@ namespace ArdalisRating.Tests
 
             Assert.Equal(0, result);
         }
+
+        [Fact]
+        public void ReturnsRatingOf10000For200000LandPolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Land,
+                BondAmount = 200000,
+                Valuation = 200000
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
+
+            var engine = new RatingEngine();
+            engine.Rate();
+            var result = engine.Rating;
+
+            Assert.Equal(10000, result);
+        }
+
+        #endregion
+
+        #region Life Policy Tests
+
+        [Fact]
+        public void ReturnsRatingOf2500For20YearOldNonSmokerOn25000LifePolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Life,
+                DateOfBirth = DateTime.Now.AddYears(-20),
+                Amount = 25000,
+                IsSmoker = false
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
+
+            var engine = new RatingEngine();
+            engine.Rate();
+            var result = engine.Rating;
+
+            Assert.Equal(2500, result);
+        }
+
+        [Fact]
+        public void ReturnsRatingOf5000For20YearOldSmokerOn25000LifePolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Life,
+                DateOfBirth = DateTime.Now.AddYears(-20),
+                Amount = 25000,
+                IsSmoker = true
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
+
+            var engine = new RatingEngine();
+            engine.Rate();
+            var result = engine.Rating;
+
+            Assert.Equal(5000, result);
+        }
+
+        #endregion
     }
 }
---
+++ ArdalisRating/AutoPolicyRater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ArdalisRating
+{
+    public class AutoPolicyRater
+    {
+        private readonly RatingEngine _engine;
+        private readonly ConsoleLogger _logger;
+
+        public AutoPolicyRater(RatingEngine engine, ConsoleLogger logger)
+        {
+            _engine = engine;
+            _logger = logger;
+        }
+
+        public void Rate(Policy policy)
+        {
+            _logger.Log("Rating AUTO policy...");
+            _logger.Log("Validating policy.");
+            if (String.IsNullOrEmpty(policy.Make))
+            {
+                _logger.Log("Auto policy must specify Make");
+                return;
+            }
+            if (policy.Make == "BMW")
+            {
+                if (policy.Deductible < 500)
+                {
+                    _engine.Rating = 1000m;
+                }
+                else
+                {
+                    _engine.Rating = 900m;
+                }
+            }
+        }
+    }
+}
