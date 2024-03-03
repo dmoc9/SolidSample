a5b4c0aaea30cd248252f5ec1ea3f3cba0297365
Brian Hodgman <hodgie@gmail.com>
2024-03-03 11:36:16 +1100

    Added test for JsonPolicySerializer

---
+++ ArdalisRating.Tests/JsonPolicySerializerTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace ArdalisRating.Tests
+{
+    public class JsonPolicySerializerTests
+    {
+        [Fact]
+        public void ReturnsDefaultPolicyFromEmptyJsonString()
+        {
+            var inputJson = "{}";
+            var serializer = new JsonPolicySerializer();
+
+            var result = serializer.GetPolicyFromJsonString(inputJson);
+
+            var policy = new Policy();
+            Assert.Equal(policy.Type, result.Type);
+        }
+    }
+}
